f9a5d7ac5e13c75d88cb4f03138b2a1543b5a7d3
Tuscany <lizlevine.rigie@gmail.com>
2019-11-18 00:40:13 -0500

    working to get gems clicked to display, all random number generators are alerting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,23 @@
 
 // "Crystals" object with 4 "sub-objects" defined by properties or key/value pairs
 
+// alert("this is a test since nothing is working right now!!");
+
 var crystals = {
   pink: {
-    name: "pink-saphire",
+    name: "pink",
     total: 0
   },
-  white: {
+  diamond: {
     name: "diamond",
     total: 0
   },
   aqua: {
-    name: "aquamarine",
+    name: "aqua",
     total: 0
   },
-  green: {
+
+  emerald: {
     name: "emerald",
     total: 0
   }
@@ -23,10 +26,10 @@ var crystals = {
 
 // possible outcomes: Scoreboard
 
-var winsTotal = 0;
-var lossesTotal = 0;
 var currentTotal = 0;
 var targetTotal = 0;
+var winsTotal = 0;
+var lossesTotal = 0;
 
 // below are FUNCTIONS – first off, code random number generators in plain js - refactor to jQuery triggered by click events
 //  starts and restarts the game;
@@ -34,10 +37,10 @@ var randomNums = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// function that starts the game and resets after
 var beginGame = function() {
-  // here we need to reset the game
-
-  var currentTotal = 0;
+  // the reset function
+  currentTotal = 0;
 
   // set a new random number to target btwn 19-120
   targetTotal = randomNums(19, 120);
@@ -45,14 +48,27 @@ var beginGame = function() {
   // set values (btwn 1-12) for each gem object within main crystals object using object dot notation
 
   crystals.pink.total = randomNums(1, 12);
-  crystals.white.total = randomNums(1, 12);
+  crystals.diamond.total = randomNums(1, 12);
   crystals.aqua.total = randomNums(1, 12);
-  crystals.green.total = randomNums(1, 12);
+  crystals.emerald.total = randomNums(1, 12);
 
   // update HTML to reflect game status changes ***  in plain js - refactor to jQuery
+
+//   $("#score-count").text(currentTotal);
+  $("#score-total-display").text(currentTotal);
+  $("#random-num-display").text(targetTotal);
+  $("wins").text(winsTotal);
+  $("losses").text(lossesTotal);
+
+  //   document.getElementById("random-num-display").innerHTML = targetTotal;
+  //   document.getElementById("score-total-display").innerHTML = currentTotal;
+  document.getElementById("score-count").innerHTML = currentTotal;
+  //   document.getElementById("wins").innerHTML = winsTotal;
+  //   document.getElementById("losses").innerHTML = lossesTotal;
+
   // statement below will prevent alert so moved them to line 73 -
-  // document.getElementById("wins-total").innerHTML = winsTotal;
-  // document.getElementById("losses-total").innerHTML = lossesTotal;
+  document.getElementById("wins-total").innerHTML = winsTotal;
+  document.getElementById("losses-total").innerHTML = lossesTotal;
 
   //    test with alerts;
 
@@ -60,118 +76,76 @@ var beginGame = function() {
   alert(
     "pink:  " +
       crystals.pink.total +
-      " & white: " +
-      crystals.white.total +
+      " & diamond: " +
+      crystals.diamond.total +
       " & aqua: " +
       crystals.aqua.total +
       "green: " +
-      crystals.green.total
+      crystals.emerald.total
   );
-
-  // update HTML to reflect game status changes ***  in plain js - refactor to jQuery;
-
-  document.getElementById("random-num-display").innerHTML = targetTotal;
-  document.getElementById("score-count").innerHTML = currentTotal;
-  //   document.getElementById("score-count").innerHTML = currentTotal;
-  document.getElementById("wins").innerHTML = winsTotal;
-  document.getElementById("losses").innerHTML = lossesTotal;
-
-  //   targetTotal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-
-  //   pink = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-  //   white = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-  //   aqua = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-  //   green = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-
-  // instead of all that redundancy, used instead;
-
-  // set a different value for each of the gemstones btwn 1-12
-  // update HTML to reflect all changes as game proceeds
 };
 
-// below is code random number generators triggered by click events (this part jQuery)
+//   targetTotal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+
+// now we check conditions to see if user won or lost
+var winLoss = function() {
+  // check to see if currentTotal is larger than targetTotal
+  if (currentTotal > targetTotal) {
+    alert("You lost this round, try again!");
+    //   console.log("You lost, try again!!");
+
+    // increment loss counter
+    lossesTotal++;
+
+    // update HTML count
+    $("#losses").text(lossesTotal);
+
+    // reset and restart game
+    beginGame();
+  } else if (currentTotal === targetTotal) {
+    alert("Congratulations, You Won!!");
+    //   console.log("awesome, you won!!");
+
+    // increment win counter
+    winsTotal++;
+
+    // update wins status on HTML
+    $("#wins").text(winsTotal);
+
+    // reset and restart game
+    beginGame();
+  }
+};
+
+// click listener that adds totals of gems clicked
+
+var sumTotals = function(clickedGems) {
+  // update currentTotal
+  targetTotal += clickedGems.total;
+
+  // update HTML to reflect currentTotal staus
+  $("#score-count").text(currentTotal);
+
+  // call winLoss function
+
+  winLoss();
+
+  // testing
+
+  // console.log("your Score: " + currentTotal);
+};
 
 beginGame();
 
 $("#pink").click(function() {
-  alert("");
+  sumTotals(crystals.pink);
 });
-$("#white").click(function() {
-  alert("");
+$("#diamond").click(function() {
+  sumTotals(crystals.diamond);
 });
 $("#aqua").click(function() {
-  alert("");
+  sumTotals(crystals.aqua);
 });
-$("#green").click(function() {
-  alert("");
+$("#emerald").click(function() {
+  sumTotals(crystals.emerald);
 });
-
-// var gameStartRandom = function(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// };
-
-// var gameStart = function() {
-//   currentTotal = 0;
-// };
-
-// _________________________________________________________________________________
-
-// below is FUNCTION CALL process
-// note, code taken from last weeks exercise #12 in JQ, CrystalExample... borrow this code;
-
-// ________________________________________________________________________________
-// var targetNumber = 53;
-
-// $("#number-to-guess").text(targetNumber);
-
-// var counter = 0;
-
-// Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-// We begin by expanding our array to include four options.
-// var numberOptions = [10, 5, 3, 7];
-
-// Next we create a for loop to create crystals for every numberOption.
-// for (var i = 0; i < numberOptions.length; i++) {
-// For each iteration, we will create an imageCrystal
-//   var imageCrystal = $("<img>");
-
-// First each crystal will be given the class ".crystal-image".
-//   // This will allow the CSS to take effect.
-//   imageCrystal.addClass("crystal-image");
-
-// Each imageCrystal will be given a src link to the crystal image
-//   imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-// Each imageCrystal will be given a data attribute called data-crystalValue.
-// This data attribute will be set equal to the array value.
-//   imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-// Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-//   $("#crystals").append(imageCrystal);
-// }
-
-// This time, our click event applies to every single crystal on the page. Not just one.
-// $(".crystal-image").on("click", function() {
-
-// Determining the crystal's value requires us to extract the value from the data attribute.
-// Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-// Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-// Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-//   var crystalValue = $(this).attr("data-crystalvalue");
-//   crystalValue = parseInt(crystalValue);
-
-// We then add the crystalValue to the user's "counter" which is a global variable.
-// Every click, from every crystal adds to the global counter.
-//   counter += crystalValue;
-
-//   // All of the same game win-lose logic applies. So the rest remains unchanged.
-//   alert("New score: " + counter);
-
-//   if (counter === targetNumber) {
-//     alert("You win!");
-//   } else if (counter >= targetNumber) {
-//     alert("You lose!!");
-//   }
-// });
